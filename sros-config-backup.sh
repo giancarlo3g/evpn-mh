29faafa62f6eb4e8210ec56ef63c9fc1d5f1728f
Giancarlo Garcia <giancarlo3g@gmail.com>
2024-10-23 23:31:07 +0000

    first commit

---
+++ sros-config-backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+yes | cp ./clab-evpnmh/pe1/tftpboot/config.txt ./config/pe1.cfg
+yes | cp ./clab-evpnmh/pe2/tftpboot/config.txt ./config/pe2.cfg
+yes | cp ./clab-evpnmh/pe3/tftpboot/config.txt ./config/pe3.cfg
+yes | cp ./clab-evpnmh/switch/tftpboot/config.txt ./config/switch.cfg
